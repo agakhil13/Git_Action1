1103b015b21ae2f04534b175f31ea41eed14c064
Gupta, Akhil (Contractor) <Akhil_Gupta@comcast.com>
2022-02-05 15:43:28 +0530

    Update test_python.py

--- test_python.py
+++ test_python.py
@@ -1,7 +1,17 @@
 import os
+import subprocess
 data = os.environ.get('TEST')
-fp = open('testfile.txt', 'r')
-file_data = fp.read()
-print(file_data)
+fp = open('testfile.txt', 'w')
+fp.write("THIS IS TEST INPUT FROM SCRIPT")
+fp.close()
+print("\n======================= Push changes to remote =======================")
+print("Set git config...")
+subprocess.Popen(["git", "config", "--global", "user.name", "github-actions[bot]"])
+subprocess.Popen(["git", "config", "--global", "user.email", "41898282+github-actions[bot]@users.noreply.github.com"])
+
+print("Push to remote...")
+subprocess.Popen(["git", "add", "-A"])
+subprocess.Popen(["git", "commit", "-m", "comment message"])
+subprocess.Popen(["git", "push"])
 print(data)
 print("hello world")
